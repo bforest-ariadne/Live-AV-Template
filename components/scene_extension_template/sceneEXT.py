a3e90a5e3a0d9684162e984c579283996aafec80
Ben Forest <ben@ben-forest.com>
2019-08-06 20:57:48 -0400

    format scene extension

--- components/scene_extension_template/sceneEXT.py
+++ components/scene_extension_template/sceneEXT.py
@@ -1,28 +1,29 @@
 op = op  # pylint:disable=invalid-name,used-before-assignment
 root = root  # pylint:disable=invalid-name,used-before-assignment
 
+
 class SceneExtension():
 
     def __init__(self, my_op):
         self.Me = my_op
         self.name = my_op.name
-        self.Me.store( 'Output', op('../out1') )
+        self.Me.store('Output', op('../out1'))
         self.springs = []
         self.getSprings()
         self.print('init')
-        self.States = [ 'Starting', 'Started', 'Stopping', 'Stopped' ]
-        self.State = self.Me.fetch( 'State' )
+        self.States = ['Starting', 'Started', 'Stopping', 'Stopped']
+        self.State = self.Me.fetch('State')
         self.Me.par.State.menuLabels = self.States
         self.Me.par.State.menuNames = self.States
-        self.onStopped = { 'operator': None, 'method': None }
-        self.onStarted = { 'operator': None, 'method': None }
+        self.onStopped = {'operator': None, 'method': None}
+        self.onStarted = {'operator': None, 'method': None}
         self.fadeIO = op('../fadeIO')
 
         self.print('init')
         # init scene index
         self.Index = -1
         self.perform = op('/Perform')
-        if len( self.perform.fetch('Scenes') ) > 0:
+        if len(self.perform.fetch('Scenes')) > 0:
             if self.name != 'blank':
                 self.Index = self.perform.fetch('Scenes').index(self.Me)
             else:
@@ -52,9 +53,9 @@ class SceneExtension():
     def Start(self, operator=None, method=None, inTime=None, outTime=None):
         # method that starts the scene
         if self.State == 'Stopped':
-            self.State = 'Starting' 
+            self.State = 'Starting'
             self.print('starting')
-            self.onStarted = { 'operator': operator, 'method': method }
+            self.onStarted = {'operator': operator, 'method': method}
             # - reset physics
             self.enableNodes()
             fadeIO = self.fadeIO
@@ -68,13 +69,14 @@ class SceneExtension():
 
     def Stop(self, operator=None, method=None):
         if self.State == 'Started':
-            self.State = 'Stopping' 
+            self.State = 'Stopping'
 
             self.print('stopping')
-            self.onStopped = { 'operator': operator, 'method': method }
+            self.onStopped = {'operator': operator, 'method': method}
             # - fade out scene from black
             fadeIO = self.fadeIO
-            fadeOutSuccess = fadeIO.Fadeout(self, 'OnFadeOut', self.Me.par.Fadeout)
+            fadeOutSuccess = fadeIO.Fadeout(
+                self, 'OnFadeOut', self.Me.par.Fadeout)
             if fadeOutSuccess == False:
                 self.OnFadeOut()
 
@@ -89,21 +91,21 @@ class SceneExtension():
         # - update started state in storage
         self.State = 'Started'
         # - run a 'started' callback
-        self.callback( self.onStarted )
+        self.callback(self.onStarted)
         return
 
     def OnFadeOut(self):
         self.print('onfadeout')
         self.finishStopping()
         return
-    
+
     def finishStopping(self):
         # - stop any other processing
         self.disableNodes()
         # - update states in storage
-        self.State = 'Stopped' 
+        self.State = 'Stopped'
         # - run stopped callback
-        self.callback( self.onStopped )
+        self.callback(self.onStopped)
         return
 
     def disableNodes(self):
@@ -126,18 +128,18 @@ class SceneExtension():
         findSpring = op('opfind_spring')
         springPaths = findSpring.cells('spring*', 'path')
         self.springs = []
-        for paths in springPaths:    
-            self.springs.append( op( paths ))
+        for paths in springPaths:
+            self.springs.append(op(paths))
         return
-    
+
     def getSelInputs(self):
         findSelInput = op('opfind_selInput')
         selInputPaths = findSelInput.cells('select*', 'path')
         self.selInputs = []
-        for paths in selInputPaths:    
-            self.selInputs.append( op( paths ))
+        for paths in selInputPaths:
+            self.selInputs.append(op(paths))
         return
-        
+
     def Lockfades(self):
         self.Me.par.Fadein.readOnly = self.Me.par.Lockfades.val
         self.Me.par.Fadeout.readOnly = self.Me.par.Lockfades.val
@@ -146,45 +148,45 @@ class SceneExtension():
     @property
     def State(self):
         return self.Me.fetch('State')
-    
+
     @State.setter
     def State(self, val):
         if val in self.States:
-            self.Me.store( 'State', val )
+            self.Me.store('State', val)
             self.Me.par.State.val = val
 
     @property
     def Index(self):
-        return self.Me.fetch( 'Index' )
-    
+        return self.Me.fetch('Index')
+
     @Index.setter
     def Index(self, val):
-        self.Me.store( 'Index', val )
+        self.Me.store('Index', val)
         self.Me.par.Index.val = val
-        
+
     def OnPulse(self, par):
-        if hasattr( self.Me, par.name ):
-            function = getattr( self.Me, par.name )
+        if hasattr(self.Me, par.name):
+            function = getattr(self.Me, par.name)
             function()
         return
 
     def OnValueChange(self, par):
-        self.Me.store( par.name, par.eval() )
-        if hasattr( self.Me, par.name ):
-            function = getattr( self.Me, par.name )
-            if callable( function ):
+        self.Me.store(par.name, par.eval())
+        if hasattr(self.Me, par.name):
+            function = getattr(self.Me, par.name)
+            if callable(function):
                 function()
         return
 
     def print(self, message):
-        print( self.name + ': ', message )
+        print(self.name + ': ', message)
         return
 
     def callback(self, config):
         if config['operator'] and config['method']:
-            if hasattr( config['operator'], config['method'] ):
+            if hasattr(config['operator'], config['method']):
                 function = getattr(config['operator'], config['method'])
-                if callable( function ):
+                if callable(function):
                     function()
             else:
                 self.print('FADEIO: callback no fire')
