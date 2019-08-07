6b6a840d1e75900e1057bc100e2c17348d4a40b9
Ben Forest <ben@ben-forest.com>
2019-08-07 15:46:40 -0400

    add test 2d sliders for key control

--- components/Calibrate/calibrateEXT.py
+++ components/Calibrate/calibrateEXT.py
@@ -6,6 +6,16 @@ class CalibrateExtension(PreShowExtension):
 
     def __init__(self, my_op):
         super().__init__(my_op)
+        self.Keys = {
+            '0' : {'u': 0, 'v' : 0},
+            '1' : {'u': 0, 'v' : 0},
+            '2' : {'u': 0, 'v' : 0},
+            '3' : {'u': 0, 'v' : 0}
+        }
+        self.xy1keyOffset = self.Me.op('widget_keyControl/slider2D_keyOffset1')
+        self.keyDat = self.Me.op('calibration/keyOffset')
+
+        # self.xy1keyControl = self.Me.op('widget_keyControl/slider2D_keyControl1')
 
         return
 
@@ -14,3 +24,11 @@ class CalibrateExtension(PreShowExtension):
         self.print('test extension Calibreate Class')
         return
 
+    def onStonerKeyChange(self, dat, cells, prev):
+        # print( dat['0','u'], dat['0', 'v'])
+        self.xy1keyOffset.par.Value0 = dat['0','u']
+        self.xy1keyOffset.par.Value1 = dat['0','v']
+        return
+
+    def OnsliderChange(self, channel, val ):
+        self.keyDat[ tdu.digits(channel.name), channel.name[-1:] ] = val
