0e03427683a8b605829386ecc2ee0e77d8b2d934
Ben Forest <ben@ben-forest.com>
2019-08-11 12:10:00 -0400

    add midi const create to init

--- components/bfPreset/presetEXT.py
+++ components/bfPreset/presetEXT.py
@@ -7,10 +7,34 @@ class PresetEXT():
     def __init__(self, my_op):
         self.Me = my_op
         self.name = my_op.name
+        self.parent = self.Me.parent()
+        self.SceneName = self.parent.name
+        self.Me.par.Scenename = self.SceneName
 
         self.print('init')
+        self.Dev()
         return
 
+    def createMidiConstant(self):
+        children = self.parent.findChildren(name='midi', type=constantCHOP)
+        if children == []:
+            print('create midi')
+            self.Midi = self.parent.create(constantCHOP, 'midi')
+            self.Midi.nodeX = self.Me.nodeX
+            self.Midi.nodeY = self.Me.nodeY - self.Me.nodeHeight - 20
+            self.parent.par.iopshortcut2 = 'midi'
+            self.parent.par.iop2 = self.Midi.path
+        elif len(children) == 1:
+            self.Midi = children[0]
+        return
+
+    def Dev(self):
+        if self.Me.fetch('Dev'):
+            self.print('dev mode on')
+            self.CreateMidiConstant = self.createMidiConstant
+        else:
+            self.print('dev mode off')
+        
 
     def Test(self):
         self.print('test extension')
