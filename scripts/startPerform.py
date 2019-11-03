6b663c3757c15ab5695a22d42001b35f7c8db925
Ben Forest <ben@ben-forest.com>
2019-11-03 17:43:56 -0500

    Create startPerform.py

---
+++ scripts/startPerform.py
@@ -0,0 +1,12 @@
+import sys,os
+
+my_path = os.path.abspath(os.path.dirname(__file__))
+toe_file = os.path.join(my_path, "../Live-AV-Template.toe")
+
+toe_env_var = 'STARTUP'
+
+toe_env_val = 'perform'
+
+os.environ[toe_env_var] = toe_env_val
+os.startfile(toe_file)
+print("starting file {} with env val {}".format(toe_file, toe_env_val))
